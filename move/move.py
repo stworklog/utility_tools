81656577d2e9ab4ff009c87d5996f4eed7030b02
stworklog <104875559+stworklog@users.noreply.github.com>
2024-09-07 00:04:08 -0400

    Use win32 api rather than pyautogui

--- move/move.py
+++ move/move.py
@@ -1,17 +1,30 @@
 import time
 import numpy as np
-import pyautogui
+# import pyautogui
+import win32api, win32con # install via: conda install pywin32
 
 def move_mouse():
-    BASE_PAUSE = 2
-    BASE = 10
-    rand_x = np.random.randint(0, 1000)
-    rand_y = np.random.randint(0, 600)
-    move_time = np.random.uniform(0.5, 2)
-    pyautogui.moveTo(BASE + rand_x, BASE + rand_y, duration=move_time)
-    print("Moved")
-    pause_duration = BASE_PAUSE + np.random.uniform(0, 10)
-    time.sleep(pause_duration)
+    # BASE_POSITION = 10
+    # rand_x = np.random.randint(0, 800)
+    # rand_y = np.random.randint(0, 600)
+    # move_time = np.random.uniform(0.5, 2)
+    # pyautogui.moveTo(BASE_POSITION + rand_x, BASE_POSITION + rand_y, duration=move_time)
+    # x = BASE_POSITION + rand_x
+    # y = BASE_POSITION + rand_y
+    win32api.SetCursorPos((np.random.randint(100, 500), np.random.randint(100, 500)))
+    time.sleep(3)
+    x = 160
+    y = 600
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+    print("Clicked at: ", x, y)
 
 while (True):
     move_mouse()
+    i = 0
+    i_total = np.random.uniform(10, 20)
+    while (i < i_total):
+        time.sleep(1)
+        i += 1
+        print("Waiting for: ", i, " seconds out of ", i_total)
